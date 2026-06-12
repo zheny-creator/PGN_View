63c63307f1bccd1384ce873601e75861f8af05d7
zheny-creator <rimkamix0@gmail.com>
2026-06-12 14:59:49 +0300

    add function stubs for PGN header parsing

---
+++ include/pgn.h
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void parse_name() {
+
+}
+
+void parse_date() {
+
+}
+
+void parse_players() {
+
+}
+
+void parse_elo() {
+    
+}
