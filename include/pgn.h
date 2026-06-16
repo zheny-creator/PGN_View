9344e02769dc42e872b62ce8841b506d12981105
zheny-creator <rimkamix0@gmail.com>
2026-06-16 12:16:42 +0300

    implement parse_date function and fix formatting

--- include/pgn.h
+++ include/pgn.h
@@ -11,11 +11,11 @@ void parse_name(char *buffer, char *name_1[], char *name_2[]) {
     {
        if (strncmp(line, "[Black ", 7) == 0)
         {
-            sscanf(line,"[Black \"%63[^\"]\"]",name_1);
+            sscanf(line, "[Black \"%63[^\"]\"]", name_1);
         }
         else if (strncmp(line, "[White ", 7) == 0)
         {
-            sscanf(line,"[White \"%63[^\"]\"]",name_2);
+            sscanf(line, "[White \"%63[^\"]\"]", name_2);
         }
 
         while (*line && *line != '\n')
@@ -26,9 +26,20 @@ void parse_name(char *buffer, char *name_1[], char *name_2[]) {
     }
     
 }
-    void parse_date()
-    {
+    void parse_date(char *buffer, char data[]) {
+    const char *line = buffer;
+    while (*line) {
+        if (strncmp(line, "[Date ", 5) == 0)
+        {
+            sscanf(line, "[Date \"%63[^\"]\"]", data);
+        }
+       while (*line && *line != '\n')
+            line++;
+
+        if (*line == '\n')
+            line++;
     }
+}
 
 void parse_players() {
 
